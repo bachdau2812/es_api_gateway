c3ae33526bcbc64a6bd02281c3e2902cf49e51c7
bachdau2812 <bachna204@gmail.com>
2025-09-05 20:39:00 +0700

    Build 1

--- src/test/java/com/dauducbach/api_gateway/ApiGatewayApplicationTests.java
+++ src/test/java/com/dauducbach/api_gateway/ApiGatewayApplicationTests.java
@@ -2,9 +2,21 @@ package com.dauducbach.api_gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApiGatewayApplicationTests {
+
+	@MockitoBean
+    public ReactiveRedisConnectionFactory reactiveRedisConnectionFactory;
+
+	@MockitoBean
+	public ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
 
 	@Test
 	void contextLoads() {
